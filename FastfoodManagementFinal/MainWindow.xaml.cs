d80437af5d62f26d8da1bd084f030f187907093f
Admin <tranthanhhien123bt@gmail.com>
2022-09-27 21:19:22 +0700

    Mô hình MVVM

--- FastfoodManagementFinal/MainWindow.xaml.cs
+++ FastfoodManagementFinal/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace FastfoodManagementFinal
     /// </summary>
     public partial class MainWindow : Window
     {
+        //http://materialdesigninxaml.net/home
         public MainWindow()
         {
             InitializeComponent();
---
+++ FastfoodManagementFinal/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FastfoodManagementFinal.ViewModel;
+
+namespace FastfoodManagementFinal.ViewModel
+{
+    public class MainViewModel : BaseViewModel 
+    {
+        // mọi thứ xử lý nằm trong này
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
